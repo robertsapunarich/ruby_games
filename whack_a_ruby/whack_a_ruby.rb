3c0db4a8e1a7694caa54cc8d6bdb56884ef9cc80
Robert Sapunarich <robertsapunarich@gmail.com>
2016-05-25 15:56:41 -0700

    Add `update` method.

--- whack_a_ruby/whack_a_ruby.rb
+++ whack_a_ruby/whack_a_ruby.rb
@@ -9,6 +9,15 @@ class WhackARuby < Gosu::Window
     @y = 200
     @width = 50
     @height = 43
+    @velocity_x = 5
+    @velocity_y = 5
+  end
+
+  def update
+    @x += @velocity_x
+    @y += @velocity_y
+    @velocity_x *= -1 if @x + @width / 2 > 1000 || @x - @width / 2 < 0
+    @velocity_y *= -1 if @y + @height / 2 > 600 || @y - @height / 2 < 0
   end
 
   def draw
