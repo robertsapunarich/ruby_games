ef079110b99ce3b9cb39842a892094b494eed95b
Robert Sapunarich <robertsapunarich@gmail.com>
2016-05-25 15:42:40 -0700

    Add `initialize` method to `WhackARuby` class. Begin work on `draw` method.

--- whack_a_ruby/whack_a_ruby.rb
+++ whack_a_ruby/whack_a_ruby.rb
@@ -1,5 +1,20 @@
 require 'gosu'
 
 class WhackARuby < Gosu::Window
+  def initialize
+    super(1000, 600)
+    self.caption = 'Whack the Ruby!'
+    @image = Gosu::Image.new('ruby.png')
+    @x = 200
+    @y = 200
+    @width = 50
+    @height = 43
+  end
 
+  def draw
+    
+  end
 end
+
+window = WhackARuby.new
+window.show
